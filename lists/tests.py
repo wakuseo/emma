6f9d4b97b2789529a9b0702d8582057782da1043
wakuseo <philipwaku@gmail.com>
2017-03-25 04:40:25 +0100

    Refctor home page to use template

--- lists/tests.py
+++ lists/tests.py
@@ -1,19 +1,11 @@
-from django.core.urlresolvers import resolve
+#from django.core.urlresolvers import resolve
 from django.test import TestCase
-from django.http import  HttpRequest
-from lists.views import home_page
+#from django.http import  HttpRequest
+#from lists.views import home_page
+#from django.template.loader import render_to_string
 
 # Create your tests here.
 class HomePageTest(TestCase):
-
-    def test_root_url_resolve_to_home_page_view(self):
-        found = resolve('/')
-        self.assertEqual(found.func, home_page)
-
-    def test_home_page_returns_correct_html(self):
-        request = HttpRequest()
-        response = home_page(request)
-        self.assertTrue(response.content.startswith(b'<html>'))
-        self.assertIn(b'<title>To-Do lists</title>', response.content)
-        self.assertTrue(response.content.endswith(b'</html>'))
-
+    def test_uses_home_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
